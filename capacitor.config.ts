972d9f7f11e89a35007756b31a35530415931246
Oguzcan Yildirim <oguzcanyildirim91@gmail.com>
2024-09-08 19:35:47 +0200

    set up tabs and navigation structure

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'german-learning-app',
+  appId: 'com.oguzhoca.germanlearning',
+  appName: 'Deutsch Lernen',
   webDir: 'www'
 };
 
